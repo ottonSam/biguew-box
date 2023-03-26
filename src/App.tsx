bcdc8528667d9cc7eab64fcc8fbed27ec26400e4
Samuel Otton <ottonsam@gmail.com>
2023-03-25 22:53:24 -0300

    use theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+import { ThemeProvider } from "styled-components";
+import dark from "./styles/themes/dark";
+
+import GlobalStyle from "./styles/global";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Opa my friend, hello world!</h1>
-    </div>
+    <ThemeProvider theme={dark}>
+      <GlobalStyle />
+      <div className="App">
+        <h1>Opa my friend, hello world!</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
